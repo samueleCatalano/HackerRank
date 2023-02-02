e1c38e7c7fa5db563fbd677c4d105977dc320a04
Samuele Catalano <csamuele73@gmail.com>
2023-02-02 15:54:06 +0100

    exercises not done: can you access

---
+++ src/can_you_access/DoNotTerminate.java
@@ -0,0 +1,23 @@
+package can_you_access;
+
+import java.security.Permission;
+
+class DoNotTerminate { //This class prevents exit(0)
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
