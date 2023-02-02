aceb3cdccb4fa0242bdb3352e0b4447f00a07177
Samuele Catalano <csamuele73@gmail.com>
2023-02-02 10:46:25 +0100

    exercise not done: java reflection

---
+++ src/java_reflection/Solution.java
@@ -0,0 +1,23 @@
+package java_reflection;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(int i=0;i<methodList.size();i++){
+            methodList.add(String.valueOf(methods));
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+}
---
+++ src/java_reflection/Student.java
@@ -0,0 +1,27 @@
+package java_reflection;
+
+class Student{
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
